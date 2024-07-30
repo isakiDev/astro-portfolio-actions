6d7595f549dfd2ca2c06b9afdda69cfe79eee73d
isakidev <gaspar.c.developer@gmail.com>
2024-07-30 01:55:18 -0400

    feat: fill mock with data

--- src/data/projects.mock.ts
+++ src/data/projects.mock.ts
@@ -1,10 +1,39 @@
 export const mockProjects = [
   {
+    id: 1,
     title: 'Fashion Like',
     date: 'feb 2024 - abr 2024',
-    description: 'Una plataforma social dedicada al mundo de la moda, donde los usuarios pueden exhibir sus estilos más destacados!',
-    skills: ['NestJs, TypeScript, Zustand, Cloudinary'],
-    imageUrl: '',
-    deployUrl: ''
+    description: 'fsdfsdfsdf',
+    // description: 'Una plataforma social dedicada al mundo de la moda, donde los usuarios pueden exhibir sus estilos más destacados!',
+    skills: ["NestJS","TypeScript","React","Docker","Cloudinary"],
+    urlImage: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png',
+    urlRepository: 'https://github.com/isakiDev/nest-fashion-like',
+    urlDeploy: 'https://isakidev-fashion-like.netlify.app'
+  },
+  {
+    id: 2,
+    title: 'Buscador de películas',
+    description: 'Integración de API movies y debounce.',
+    skills: ["React","JavaScript","CSS"],
+    urlDeploy: 'https://react-search-movies.isakidev.com',
+    urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1701655988/portfolio/ckkxmvb2ym42sdggzqvp.png'
+  },
+  {
+    id: 3,
+    title: 'Calendar',
+    description: 'Gestionador de notas con redux y autenticación de usuarios.',
+    urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1705917516/portfolio/xojyy8odx2mnyc8uleeh.png',
+    skills: ["Node.js","TypeScript","React","Redux","Tailwind CSS","MongoDB"],
+    urlRepository: 'https://github.com/isakiDev/node-calendar-ts',
+    urlDeploy: 'https://react-calendar.isakidev.com/auth/login',
+  },
+  {
+    id: 4,
+    title: 'Todo List',
+    urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1701660260/portfolio/gnexvarptbkebssqc3k7.png',
+    description: 'Gestionador de tareas',
+    skills: ["React","TypeScript","Tailwind CSS"],
+    urlRepository: 'https://github.com/isakiDev/react-todo-ts',
+    urlDeploy: 'https://react-todots.isakidev.com',
   }
 ]
