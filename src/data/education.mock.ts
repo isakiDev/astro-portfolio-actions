a24925cf1f3cbbc4dbcc3b69299ebd129b7471a5
isakidev <gaspar.c.developer@gmail.com>
2024-07-30 01:54:48 -0400

    feat: add education mock

---
+++ src/data/education.mock.ts
@@ -0,0 +1,9 @@
+import type { TimeLineData } from "../components/ui/TimeLine/time-line.interface";
+
+export const mockEducation: TimeLineData[] = [
+  {
+    subTitle: { title: "Instituto Profesional AIEP", url: "https://www.linkedin.com/school/instituto-profesional-aiep/"},
+    date: "abr 2019 - abr 2023",
+    title: "Ingeniería de Ejecución en Informática mención Desarrollo de Sistemas",
+  },
+];
