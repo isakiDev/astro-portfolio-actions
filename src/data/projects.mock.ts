4e606f3eef0c4bfbd31a903c90710f340389ab9d
isakidev <gaspar.c.developer@gmail.com>
2024-08-04 19:01:06 -0400

    chore: update mock

--- src/data/projects.mock.ts
+++ src/data/projects.mock.ts
@@ -12,7 +12,7 @@ export const mockProjects: Project[] = [
   {
     title: 'Fashion Like',
     date: 'feb 2024 - abr 2024',
-    description: 'Aplicación dedicada al mundo de la moda, donde los usuarios pueden exhibir sus estilos más destacados.',
+    description: 'Red social dedicada al mundo de la moda, donde los usuarios pueden exhibir sus estilos más destacados.',
     skills: ["NestJS","TypeScript","React","Docker","Cloudinary", "PostgreSQL", "Zustand"],
     urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1722641869/portfolio/bhppoums61vaagcohi1f.png',
     urlRepository: 'https://github.com/isakiDev/nest-fashion-like',
@@ -20,7 +20,7 @@ export const mockProjects: Project[] = [
   },
   {
     title: 'Calendar',
-    description: 'Gestionador de notas con redux y autenticación de usuarios.',
+    description: 'Aplicación web para agendar notas en un calendario atractivo, permitiendo a los usuarios visualizar el flujo y las fechas establecidas.',
     urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1722633286/portfolio/nusbxa2suly7h1n0ctay.png',
     skills: ["Node.js","TypeScript","React","Redux","TailwindCSS","MongoDB", "Clean Architecture"],
     urlRepository: 'https://github.com/isakiDev/node-calendar-ts',
@@ -36,7 +36,7 @@ export const mockProjects: Project[] = [
   {
     title: 'Todo List',
     urlImage: 'https://res.cloudinary.com/dzn3nempv/image/upload/v1722633570/portfolio/dn8fqcnuhagjkqpbmrhb.png',
-    description: 'Gestionador de tareas',
+    description: 'Gestor de tareas diarias diseñado para mejorar la eficiencia de los usuarios. Facilita la organización y priorización de actividades.',
     skills: ["React","TypeScript","TailwindCSS"],
     urlRepository: 'https://github.com/isakiDev/react-todo-ts',
     urlDeploy: 'https://isakidev-todo.netlify.app/',
