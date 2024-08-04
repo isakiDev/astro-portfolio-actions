fc7ab7cc20d534543f001b1c98f9342d4c35fbda
isakidev <gaspar.c.developer@gmail.com>
2024-08-03 22:26:03 -0400

    feat: add modifications to improve the performance

--- src/data/socials.mock.ts
+++ src/data/socials.mock.ts
@@ -4,14 +4,17 @@ import TikTok from "@icons/TikTok.astro";
 
 export const mockSocials = [
   {
+    name: 'GitHub',
     icon: GitHub,
     href: "https://github.com/isakiDev",
   },
   {
+    name: 'LinkedIn',
     icon: LinkedIn,
     href: "https://www.linkedin.com/in/isakidev/",
   },
   {
+    name: 'TikTok',
     icon: TikTok,
     href: "https://www.tiktok.com/@isakidev",
   },
