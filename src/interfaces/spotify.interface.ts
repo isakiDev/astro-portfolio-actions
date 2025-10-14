fbfd275e261f9bd4c6a9e362fb0e6916d31d12c6
isakidev <gaspar.c.developer@gmail.com>
2025-10-14 12:50:45 -0300

    refactor: :label: move interface

---
+++ src/interfaces/spotify.interface.ts
@@ -0,0 +1,10 @@
+export interface PlayingResponse {
+  albumImageUrl: string;
+  artist: string;
+  isPlaying: boolean;
+  songUrl: string;
+  title: string;
+  timePlayed: number;
+  timeTotal: number;
+  artistUrl: string;
+}
